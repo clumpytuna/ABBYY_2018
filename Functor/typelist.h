15179bd7f72e756dee64f081685541230bb7ce03
clumpyTuna <anuhinm@mail.ru>
2018-04-28 13:08:18 +0300

    Update typelist.h

--- Functor/typelist.h
+++ Functor/typelist.h
@@ -20,16 +20,22 @@ class CNullTypeList {};
 
 template <class TList, unsigned int index, typename DefaultType = CNullTypeList>
 class TypeAtNonStrict {
+
+public:
     typedef DefaultType Result;
 };
 
 template <class Head, class Tail, typename DefaultType>
 class TypeAtNonStrict<CTypeList<Head, Tail>, 0, DefaultType> {
+
+public:
     typedef Head Result;
 };
 
 template <class Head, class Tail, unsigned int i, typename DefaultType>
 class TypeAtNonStrict<CTypeList<Head, Tail>, i, DefaultType> {
+
+public:
     typedef typename TypeAtNonStrict<Tail, i - 1, DefaultType>::Result Result;
 };
 
