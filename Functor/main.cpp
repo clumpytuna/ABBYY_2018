af8f4e911eb3b2b94bf5f8990bae1c4845ae466e
Michael Anukhin <anuhinm@mail.ru>
2018-05-04 14:42:06 +0300

    add new test classes

--- Functor/main.cpp
+++ Functor/main.cpp
@@ -10,17 +10,45 @@ public:
     }
 };
 
-void function(int i, double d) {
-    std::cout << "Function call" << std::endl;
+void firstFunction(int i, double d) {
+    std::cout << "Function call with arguments: int:" << i << ", double:" << d << std::endl;
 }
+
+void secondFunction() {
+    std::cout << "Function call without arguments" << std::endl;
+}
+
+class Object {
+public:
+    void DoNothing() const {
+        std::cout << "Do nothing!" << std::endl;
+    }
+    void DoSomething() {
+        ++counter;
+        std::cout << "Increase the counter: " << counter << std::endl;
+    }
+private:
+    int counter = 0;
+};
 
 int main()
 {
-    CFunctor<void, TYPELIST_2(int, double)> f(&function);
-    f(4, 4.5);
+    CFunctor<void, TYPELIST_2(int, double)> function(&firstFunction);
+    function(4, 4.5);
+    
     // Можно оборачивать не только функции,
     // но и другие классы с переопределенным оператором ().
     Functor functor;
-    f = functor;
-    f(5, 4.0);
+    function = functor;
+    function(5, 4.0);
+    
+    // Также можно привязывать методы классов
+    Object object;
+    CFunctor<void, NullType> action(&object, &Object::DoNothing);
+    action();
+    // А затем привязывать другой метод
+    action.BindMethod(&object, &Object::DoSomething);
+    action();
+    
 }
+
